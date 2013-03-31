71dd168879a98c1ccba3124b1e7e2bd0c6d2f541
Syohei YOSHIDA <syohex@gmail.com>
2013-03-31 23:53:34 +0900

    Add version meta information

--- emamux.el
+++ emamux.el
@@ -4,6 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-emamux
+;; Version: 0.01
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
