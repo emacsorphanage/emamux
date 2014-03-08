d030aa007ff4939fc1ec41ba29c0e1623bbfbff8
Syohei YOSHIDA <syohex@gmail.com>
2014-03-08 09:59:44 +0900

    Update requirement cl-lib version

--- emamux.el
+++ emamux.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-emamux
 ;; Version: 0.05
-;; Package-Requires: ((cl-lib "0.3"))
+;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
