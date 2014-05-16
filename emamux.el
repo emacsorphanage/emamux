9d1a672c61206bbde1280b6edd07cf7e1e822814
Syohei YOSHIDA <syohex@gmail.com>
2014-05-16 14:16:24 +0900

    Bump up version and update Changes

--- emamux.el
+++ emamux.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-emamux
-;; Version: 0.07
+;; Version: 0.08
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
