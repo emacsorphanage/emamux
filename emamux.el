625d93afad6a689b1c015599796835c82ee00838
Syohei YOSHIDA <syohex@gmail.com>
2016-11-23 21:10:21 +0900

    Drop Emacs 24.1 and 24.2 support

--- emamux.el
+++ emamux.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-emamux
 ;; Version: 0.13
-;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
+;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
